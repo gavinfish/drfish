381bea71ec214eba3c1b52b5986f522455d3c335
drfish <drfish.me@gmail.com>
2019-10-11 11:13:08 +0800

    Init windows setup script

---
+++ setup-windows.ps1
@@ -0,0 +1,14 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install vscode --yes
+choco install git --yes
+choco install golang --yes
+choco install maven --yes
+choco install sourcetree --yes
+choco install intellijidea-community --yes
+choco install googlechrome --yes
+choco install jdk8 --yes
+choco install goland --yes
+choco install wechat --yes
+choco install conemu --yes
+choco install adobereader --yes

c56d8f82bc7264770dce9c4defe9088145b6b04b
Jie Shen <drfish.me@gmail.com>
2019-11-19 14:34:34 +0800

    Install python for win (#4)

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -3,11 +3,12 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 choco install vscode --yes
 choco install git --yes
 choco install golang --yes
+choco install jdk8 --yes
+choco install python --yes
 choco install maven --yes
 choco install sourcetree --yes
 choco install intellijidea-community --yes
 choco install googlechrome --yes
-choco install jdk8 --yes
 choco install goland --yes
 choco install wechat --yes
 choco install conemu --yes

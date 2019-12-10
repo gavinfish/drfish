60c5b70bc54cacf899fddc5f9f41999e83e94374
drfish <drfish.me@gmail.com>
2019-12-10 23:04:34 +0800

    Init set up mac os script

---
+++ setup-macos.sh
@@ -0,0 +1,16 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew cask install google-chrome
+brew cask install shadowsocksx-ng
+brew cask install visual-studio-code
+brew cask install intellij-idea
+brew cask install pycharm
+brew cask install flux
+brew cask install iterm2
+brew cask install wechat
+brew cask install neteasemusic
+brew cask install qq
+brew cask install sourcetree
+
+brew cask install adoptopenjdk8
+brew install maven
+brew intall go

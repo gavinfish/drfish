1859922f019d5d291672fae13885025ca4804b0d
Jie Shen <drfish.me@gmail.com>
2020-01-04 22:30:05 +0800

    Install GNU tools for mac (#9)

--- setup-macos.sh
+++ setup-macos.sh
@@ -4,13 +4,45 @@ brew cask install shadowsocksx-ng
 brew cask install visual-studio-code
 brew cask install intellij-idea
 brew cask install pycharm
+brew cask install goland
+brew cask install docker
 brew cask install flux
 brew cask install iterm2
 brew cask install wechat
 brew cask install neteasemusic
 brew cask install qq
 brew cask install sourcetree
-
 brew cask install adoptopenjdk8
+
 brew install maven
-brew intall go
+brew install go
+brew install jq
+
+# install GNU Command Line Tools
+brew install coreutils
+brew install binutils
+brew install diffutils
+brew install ed
+brew install findutils
+brew install gawk
+brew install gnu-indent
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-which
+brew install gnutls
+brew install grep
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff
+brew install wget
+
+# OS X ships a GNU version, but too old
+brew install bash
+brew install gdb  # gdb requires further actions to make it work. See `brew info gdb`.
+brew install gpatch
+brew install m4
+brew install make
+brew install nano
+
+xcode-select --install
